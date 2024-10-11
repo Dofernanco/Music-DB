699e031a0d2292586b26e9ccaa277bd3b992032a
Fernando Colón <fera@friday>
2024-10-11 03:42:50 -0600

    Agregué las primeras partes del proyecto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Gtk;
+
+class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        // Inicializar GTK#
+        Application.Init();
+
+        // Crear la ventana principal y ejecutar la aplicación
+        MainWindow win = new MainWindow();
+        win.Show();
+
+        // Iniciar el ciclo principal de GTK
+        Application.Run();
+    }
+}
+
